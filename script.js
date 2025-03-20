ad5c675295421ac8ed37ddfa7227b4e8af7d027b
aguskex <109179354+aguskex@users.noreply.github.com>
2025-03-20 18:34:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,18 @@ scrollToTopBtn.addEventListener("click", () => {
 });
 
 $(document).ready(function () {
-    // Inicializar el carrusel de imágenes
+    $('.carousel-titulos').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+
+$(document).ready(function () {
     $('.carousel-imagenes').slick({
         dots: true,
         infinite: true,
@@ -27,17 +38,15 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
     });
 
-    // Inicializar el carrusel de videos
     $('.carousel-videos').slick({
         dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         adaptiveHeight: true,
-        autoplay: false, // Desactivar autoplay del carrusel
+        autoplay: false,
     });
 
-    // Pausar el video anterior y reproducir el nuevo al cambiar de slide
     $('.carousel-videos').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
         // Pausar el video actual
         var currentVideo = $(slick.$slides[currentSlide]).find('video').get(0);
@@ -47,15 +56,13 @@ $(document).ready(function () {
     });
 
     $('.carousel-videos').on('afterChange', function (event, slick, currentSlide) {
-        // Reproducir el video del slide actual
         var currentVideo = $(slick.$slides[currentSlide]).find('video').get(0);
         if (currentVideo) {
             currentVideo.play();
         }
     });
 
-    // Avanzar al siguiente slide cuando el video termine
     $('.carousel-videos video').on('ended', function () {
-        $('.carousel-videos').slick('slickNext'); // Cambiar al siguiente slide
+        $('.carousel-videos').slick('slickNext');
     });
 });
